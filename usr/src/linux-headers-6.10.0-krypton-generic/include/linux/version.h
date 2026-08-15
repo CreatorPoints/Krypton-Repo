5c1c505619911e937bd2e96e91100f81a786e154
CreatorPoints <shrestangsu.dutta@gmail.com>
2026-08-15 22:52:09 +0530

    feat: Initial upstream repository release for KryptonOS 1.0.0.0 LTS

---
+++ usr/src/linux-headers-6.10.0-krypton-generic/include/linux/version.h
@@ -0,0 +1,6 @@
+#define LINUX_VERSION_CODE 395776
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define LINUX_VERSION_MAJOR 6
+#define LINUX_VERSION_PATCHLEVEL 10
+#define LINUX_VERSION_SUBLEVEL 0
+#define KRYPTON_KERNEL_NAME "krypton-generic"
